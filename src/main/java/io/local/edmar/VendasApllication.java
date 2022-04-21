e57cb3115c889514b1eb00cdc579c2e5e9a1d13f
Edmar Reis <edmar.sr@hotmail.com>
2022-04-21 08:09:07 -0300

    Initial commit

---
+++ src/main/java/io/local/edmar/VendasApllication.java
@@ -0,0 +1,59 @@
+package io.local.edmar;
+
+import io.local.edmar.domain.entity.Cliente;
+import io.local.edmar.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+//@RestController  // controladora rest, manda menssagens para o browser
+@SpringBootApplication  //classe inicializadora do projeto
+public class VendasApllication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Edmar"));
+            clientes.salvar(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                    c.setNome(c.getNome() + " atualizado. ");
+                    clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+            System.out.println("Deletando clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado");
+            }else {
+                todosClientes.forEach(System.out::println);
+            }
+        };
+
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VendasApllication.class, args);
+    }
+
+}
---
+++ src/main/java/io/local/edmar/domain/repositorio/Clientes.java
@@ -0,0 +1,60 @@
+package io.local.edmar.domain.repositorio;
+
+import io.local.edmar.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository  //faz operações na base de dados
+public class Clientes {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional  // faz uma transação na base de dados
+    public Cliente salvar(Cliente cliente){
+        entityManager.persist(cliente);
+        return cliente;
+    }
+
+    @Transactional
+    public Cliente atualizar(Cliente cliente){
+        entityManager.merge(cliente);
+        return cliente;
+    }
+
+    @Transactional
+    public void deletar(Cliente cliente){
+        if(!entityManager.contains(cliente)){
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
+    }
+
+    @Transactional
+    public void deletar(Integer id){
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        deletar(cliente);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cliente> buscarPorNome(String nome){
+        String jpql = " select c from Cliente c where c.nome like :nome ";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%"+nome+"%");
+        return query.getResultList();
+    }
+
+    @Transactional
+    public List<Cliente> obterTodos(){
+        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
+    }
+
+}
